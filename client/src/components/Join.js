bd9dd8fd0a86e508ec80f909c2ecac195a91599c
silentc1c4d4 <silentcicada3301@gmail.com>
2020-12-06 01:15:14 +0530

    Basic Client Setup

---
+++ client/src/components/Join.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Join = ()=> {
+     
+    return(
+        <p>Join</p>
+    )
+
+}
+
+export default Join
---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+// the above line means that everything we create in react will be in App and that will be forced into the div with id "root".
+
